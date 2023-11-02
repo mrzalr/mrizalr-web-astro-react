b94ad103eee43b3649bd30e0f291d42bc9da9fa7
rizal.rusdiansyah <rizal.rusdiansyah@runsystem.id>
2023-11-02 12:19:12 +0700

    ADD search bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,91 +1,104 @@
-import React from "react";
+import React, { useState } from "react";
 import { useMediaQuery } from "react-responsive";
-import logo from "../../public/mrizalr-logo-simple.png";
 
-const DesktopNav = () => {
+const Navbar = () => {
+  const [menuVisible, setMenuVisible] = useState(false);
+  const [toggleSearch, setToggleSearch] = useState(false);
+
+  const isMobile = useMediaQuery({ query: "(max-width:640px)" });
+
   return (
-    <header className="w-screen">
-      <nav className="flex p-4 justify-between items-center">
+    <>
+      <nav className="flex p-4 justify-between items-center relative bg-white">
         <a href="/" className="flex gap-2 items-center">
           <img
             className="w-6 h-6"
-            src="../../public/mrizalr-logo-simple.png"
+            src="mrizalr-logo-simple.png"
             alt="mrizalr-logo"
           />
           <h1 className="text-lg font-bold">mrizalr.</h1>
         </a>
-        <ul className="flex gap-3 lg:gap-8 w-50 justify-center">
-          <li className=" text-sm text-center">
-            <a href="/">/index</a>
-          </li>
-          <li className=" text-sm text-center">
-            <a href="/about-me">/about-me</a>
-          </li>
-          <li className=" text-sm text-center">
-            <a href="/blogs">/blogs</a>
-          </li>
-          <li className=" text-sm text-center">
-            <a href="/portofolio">/portofolio</a>
-          </li>
-        </ul>
+        {!isMobile && (
+          <ul className="flex gap-8 lg:gap-12 w-50 justify-center duration-150">
+            <li className="text-primary font-bold text-sm text-center">
+              <a href="/">/index</a>
+            </li>
+            <li className="font-bold text-sm text-center">
+              <a href="/about-me">/about-me</a>
+            </li>
+            <li className="font-bold text-sm text-center">
+              <a href="/blogs">/blogs</a>
+            </li>
+            <li className="font-bold text-sm text-center">
+              <a href="/portofolio">/portofolio</a>
+            </li>
+          </ul>
+        )}
         <div className="flex gap-4 items-center">
-          <button>
-            <i class="ri-search-line"></i>
+          <button onClick={() => setToggleSearch((toggled) => !toggled)}>
+            <i className="ri-search-line"></i>
           </button>
-          <button className="bg-lime-500 w-24 h-10 rounded-full text-white font-semibold">
+          {isMobile ? (
+            <i
+              onClick={() => setMenuVisible((visible) => !visible)}
+              className={`ri-${
+                menuVisible ? "close" : "menu"
+              }-line cursor-pointer z-50`}
+            ></i>
+          ) : (
+            <button className="bg-primary w-28 h-10 rounded-full text-defaultWhite font-semibold">
+              Subscribe
+            </button>
+          )}
+        </div>
+        <div
+          className={`absolute ${
+            isMobile ? "left-10 right-10" : "right-8 w-72"
+          } -bottom-16 bg-white ${
+            toggleSearch ? "opacity-100" : "opacity-0"
+          } rounded-full px-6 py-4 duration-150`}
+        >
+          <form className="flex gap-2 items-center">
+            <input
+              className="outline-none w-full"
+              type="text"
+              placeholder="search posts or tags"
+            />
+            <i className="ri-search-line text-gray-400"></i>
+          </form>
+        </div>
+      </nav>
+      {isMobile && (
+        <div
+          className={`bg-white fixed ${
+            menuVisible ? "left-0 right-0" : "left-full -right-full"
+          } top-0 bottom-0 font-semibold flex justify-center items-center duration-150 z-40`}
+        >
+          {/* <i
+            onClick={() => setMenuVisible(false)}
+            className={`ri-close-line cursor-pointer absolute right-4 top-4`}
+          ></i> */}
+          <ul className="flex flex-col gap-6 items-center">
+            <li>
+              <a href="/">/index</a>
+            </li>
+            <li>
+              <a href="/about-me">/about-me</a>
+            </li>
+            <li>
+              <a href="/blogs">/blogs</a>
+            </li>
+            <li>
+              <a href="/portofolio">/portofolio</a>
+            </li>
+          </ul>
+          <button className="absolute bottom-5 left-50 -translate-x-50 bg-primary w-32 h-10 rounded-full text-defaultWhite font-semibold mt-12">
             Subscribe
           </button>
         </div>
-      </nav>
-    </header>
+      )}
+    </>
   );
-};
-
-const MobileNav = () => {
-  return (
-    <header className="w-screen relative">
-      <nav className="flex p-4 justify-between">
-        <a href="/" className="flex gap-2 items-center">
-          <img
-            className="w-6 h-6"
-            src="../../public/mrizalr-logo-simple.png"
-            alt="mrizalr-logo"
-          />
-          <h1 className="text-lg font-bold">mrizalr.</h1>
-        </a>
-        <div className="flex gap-4 items-center">
-          <button>
-            <i class="ri-search-line"></i>
-          </button>
-          <i className="ri-menu-line z-50"></i>
-        </div>
-      </nav>
-      <div className="bg-white fixed top-0 left-0 right-0 bottom-0 font-semibold flex justify-center items-center hidden">
-        <ul className="flex flex-col gap-6 items-center">
-          <li>
-            <a href="/">/index</a>
-          </li>
-          <li>
-            <a href="/about-me">/about-me</a>
-          </li>
-          <li>
-            <a href="/blogs">/blogs</a>
-          </li>
-          <li>
-            <a href="/portofolio">/portofolio</a>
-          </li>
-        </ul>
-        <button className="absolute bottom-5 left-50 -translate-x-50 bg-lime-500 w-32 h-10 rounded-full text-white font-semibold mt-12">
-          Subscribe
-        </button>
-      </div>
-    </header>
-  );
-};
-
-const Navbar = () => {
-  const isMobile = useMediaQuery({ query: "(max-width: 640px)" });
-  return isMobile ? MobileNav() : DesktopNav();
 };
 
 export default Navbar;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,14 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors:{
+				primary:'#035AA6',
+				secondary: '#FCBF1E',
+				defaultWhite: '#F5F5F5',
+				defaultFont: '#120136'
+			}
+		},
 	},
 	plugins: [],
 }
